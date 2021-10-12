a6b4b38c82659866bdcc04b0edfefc94f5d59395
Andor <zeremail15@gmail.com>
2021-10-12 21:53:07 +0200

    first powershell scripts has been added

--- 7. for ciklus/for-in.js
+++ 7. for ciklus/for-in.js
@@ -5,9 +5,9 @@ let myCar = {
 };
 
 for (let k in myCar) {
-    console.log(k, myCar[k] );
-    myCar[k] = myCar[k] +"black"
+    console.log(k);
+    /* myCar[k] = myCar[k] +"black" */
 }
 
-console.log("myCar Object: ", myCar);
-console.log("myCar.make értéke: ", myCar.make);
+/* console.log("myCar Object: ", myCar);
+console.log("myCar.make értéke: ", myCar.make); */
