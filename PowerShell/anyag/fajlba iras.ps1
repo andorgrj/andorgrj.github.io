ffd1159e52f00635806c177ed9658f9b60135684
Andor <zeremail15@gmail.com>
2021-10-31 11:13:11 +0100

    file

--- PowerShell/anyag/fajlba iras.ps1
+++ PowerShell/anyag/fajlba iras.ps1
@@ -1,4 +1,4 @@
-Fájlba írás/pipe
+Fájlba írás
 
 Get-Date | Out-File -FilePath $filePath
 $filePath = "$env:USERPROFILE\Desktop\bbbbb.txt"
@@ -26,3 +26,22 @@ Get-Date | Out-File -FilePath $filePath2 -Append -Encoding utf8
 
 
 "ÉÉÉ ÍÍÍ ŐŐŐ" | Out-File -filePath $filePath2 -Append -Encoding utf8
+
+------------------------------------------------------------------------------------------------------------------------------------
+
+Get-PSDrive | Out-File C:\Users\admin\Desktop\cars.txt
+
+Start-Process notepad C:\Users\admin\Desktop\cars.txt
+
+Stop-Process -Name Notepad cars.txt
+
+Get-Content C:\Users\admin\Desktop\cars.txt
+
+$a = Get-Content C:\Users\admin\Desktop\cars.txt
+
+$a.Count
+
+Get-PSDrive | Export-Csv C:\Users\admin\Desktop\cars.csv   -----> fájlba kiírja az adatokat
+
+$serv = Import-Csv c:\Users\admin\Desktop\cars.csv -------> fájlból beolvassa az import az értékeket
+$serv.Count
