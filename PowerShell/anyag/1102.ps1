7783418f80251d78d5863b2d43d78072d1f62068
unknown <zeremail15@gmail.com>
2021-11-02 21:32:06 +0100

    test

---
+++ PowerShell/anyag/1102.ps1
@@ -0,0 +1,21 @@
+draw.io 
+
+halozati topologia retegei : core, switch 
+dhcp-t disztribucios szintre kell bekötni
+
+access layer
+
+router ad dhcp-t
+
+subnet létrehozáds s dhcp-n belül 
+
+vlan --> egy hálozaton belül több logikai cimzest hasznalhatok
+
+dns server is kell
+
+fqdn - fully qualified domain name
+
+router látja el a tűzfal szerepét - osi layeren a 3as-on legyenek, jobb tuzfal bentebb is tud menni
+
+
+
---
+++ PowerShell/anyag/masodikgyakorlo.ps1
@@ -0,0 +1,42 @@
+Get-Windowsoptionalfeature -Online | Where-Object {$_.FeatureName -like "*Windows*"}
+Disable-WindowsOptionalFeature -Online -FeatureName TelnetClient
+
+Get-Command -Noun Process
+Get-Command -Noun WindowsOptionalFeature
+
+
+Get-Process | Where-Object {$_.ProcessName -like "*powershell*"}
+Get-Service | Where-Object {$_.Status -eq "Running"}
+$a = Get-Service | Where-Object {$_.Name -eq "wmiApSrv"}
+
+Get-Service | Where-Object {$_.Name -like "*wmi*"}
+
+Set-Service -Name wmiApSrv -StartupType Disabled
+Set-Service wmiApSrv -StartupType Disabled
+Set-Service -InputObject $a -StartupType Automatic
+
+Get-Process | Where-Object {$_.ProcessName -like "*wmi*"}
+
+---------------------------------------------------------------------------------------------
+
+
+$hash = @{ Number = 1; Shape = "Square"; Color = "Blue"}         
+
+$hash = [ordered]@{ Number = 1; Shape = "Square"; Color = "Blue"}
+
+$hash.keys
+$hash.values
+$hash.count
+
+$hash.Add("Time", "Now") 
+
+$t = "Today"    
+$now = (Get-Date)
+$hash.Add($t, $now)
+
+$hash.Remove("Time")  
+
+
+----------------------------------------------------------------------------------------------
+
+Get-PSDrive | ?{$_.Free -gt 1} | %{$Count = 0; Write-Host "";} { $_.Name + ": Used: " + "{0:N2}" -f ($_.Used/1gb) + " Free: " + "{0:N2}" -f ($_.free/1gb) + " Total: " + "{0:N2}" -f (($_.Used/1gb)+($_.Free/1gb)); $Count = $Count + $_.Free;}{Write-Host"";Write-Host "Total Free Space " ("{0:N2}" -f ($Count/1gb)) -backgroundcolor green}
