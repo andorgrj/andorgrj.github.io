3383a4d6f031e5686e79611c118fa9ef9e067156
unknown <zeremail15@gmail.com>
2022-01-11 16:15:33 +0100

    python

--- python/first.py
+++ python/first.py
@@ -21,7 +21,17 @@ name = input('What is your name? ')
 print('Hi ' + name)
 
 name = input('What is your name? ')
-print('Hi ' + name)
-question = input('What is your favourite color? ')
-print('Andor likes' + question)
+favourite_color = input('What is your favourite color? ')
+print(name + 'likes ' + favourite_color)
 
+#Conversion
+
+birth_year = input('Birth year: ')
+age = 2019 - int(birth_year)
+print(age)
+
+#string convert into int, float, bool - azért, hogy szám legyen belőle és azzal tudjon számolni
+
+weight = input('How many pounds you weigh? ')
+kilograms = int(weight) * 0.453
+print(kilograms)
