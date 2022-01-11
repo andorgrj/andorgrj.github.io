9e94a70b0be252956fb8d07c1a0947756398a40a
unknown <zeremail15@gmail.com>
2022-01-11 15:52:53 +0100

    python

--- python/first.py
+++ python/first.py
@@ -1,1 +1,27 @@
 print("Andor")
+print('o----')
+print(' |||')
+print('*' * 10)
+
+
+price = 10
+rating = 4.9
+name = 'Andor'
+is_published = True
+print(price)
+
+
+full_name = 'John'
+age = 20
+is_new = True
+
+#Input from the user:
+
+name = input('What is your name? ')
+print('Hi ' + name)
+
+name = input('What is your name? ')
+print('Hi ' + name)
+question = input('What is your favourite color? ')
+print('Andor likes' + question)
+
