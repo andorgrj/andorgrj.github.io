5c821654808dfcad03fdfe612926ae91f9ef416f
unknown <zeremail15@gmail.com>
2022-01-12 21:38:38 +0100

    python

--- python/first.py
+++ python/first.py
@@ -32,6 +32,32 @@ print(age)
 
 #string convert into int, float, bool - azért, hogy szám legyen belőle és azzal tudjon számolni
 
-weight = input('How many pounds you weigh? ')
-kilograms = int(weight) * 0.453
-print(kilograms)
+weight_lbs = input('How many pounds you weigh? ')
+weight_kg = int(weight_lbs) * 0.453
+print(weight_kg)
+
+#Strings
+
+course = "Python's for beginners"
+course2 = 'Python for "beginners"'
+print(course)
+
+course = '''
+Hi negerkocka
+hello
+több sorba írható szöveg így
+'''
+
+course = 'Python for beginners'
+        #'012345'
+print(course[0])
+print(course[1:-1])
+print(course[0:-1])
+
+#Formatted strings
+
+first = 'John'
+last = 'Smith'
+message = first + ' [' + last + '] is a coder'
+msg = f'{first} [{last}] is a coder' #it is the formatted string
+print(message)
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+weight = input('How many pounds you weigh? ')
+kilograms = int(weight) * 0.453
+print(kilograms)
